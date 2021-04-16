4f7ffbb72d6c8d41c06d5f0374659bb2b42e4bf9
RyanAngJY <ryanang.jy@gmail.com>
2021-04-16 19:52:16 +0800

    cleanup display for checking if everything is working

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -2,13 +2,14 @@ import './index.scss'
 import React, { memo, useState, useEffect } from 'react'
 import request from 'utils/request'
 import REACT_APP_SOCKET_BASE_URL from 'settings/socketConfig'
-import ViraspaceLogo from 'images/viraspace_logo.png'
 import io from 'socket.io-client';
 
-class Dashboard extends React.Component {
+class SocketBoard extends React.Component {
     state = {
         socketData: "",
+        namespaceSocketData: "",
         broadcastedSocketData: "",
+        isConsumerWorking: "NO",
     }
 
     componentWillUnmount() {
@@ -26,8 +27,12 @@ class Dashboard extends React.Component {
         this.defaultSocket.on("myCustomResponseMessage", message => {
             this.setState({'socketData': message.data })
         })
+        this.defaultSocket.on("isConsumerWorking", message => {
+            console.log("isConsumerWorking")
+            this.setState({'isConsumerWorking': "YES" })
+        })
         this.namespaceSocket.on("myCustomResponseMessage", message => {
-            this.setState({'socketData': message.data })
+            this.setState({'namespaceSocketData': message.data })
         })
         this.namespaceSocket.on("broadcastedMessage", message => {
             this.setState({'broadcastedSocketData': message.data })
@@ -49,35 +54,48 @@ class Dashboard extends React.Component {
     render() {
         return (
             <>
-                <div>Data: {this.state.socketData}</div>
-                <div>Broadcasted Data: {this.state.broadcastedSocketData}</div>
+                <p><span className="field">Is Consumer Working: </span>{this.state.isConsumerWorking}</p>
+                <p><span className="field">Data: </span>{this.state.socketData}</p>
+                <p><span className="field">Namespace Data: </span>{this.state.namespaceSocketData}</p>
+                <p><span className="field">Broadcasted Data: </span>{this.state.broadcastedSocketData}</p>
                 <button onClick={this.emitMessage}> Emit Message </button>
                 <button onClick={this.emitCustomMessage}> Emit Custom Message </button>
-                <button onClick={this.emitToNamespace}> Emit To Namespace </button>
+                <button  onClick={this.emitToNamespace}> Emit To Namespace </button>
             </>
         )
     }
 }
 
 const Home = props => {
-    const [data, setData] = useState("")
+    const [db, setDb] = useState("")
+    const [lib, setLib] = useState("")
+    const [microservice, setMicroservice] = useState("")
+    const [kafkaTopic, setKafkaTopic] = useState("")
     const [isUploading, setIsUploading] = useState(false)
     const [images, setImages] = useState([])
 
+    async function fetchData() {
+        request
+            .get('/')
+            .then(res => {
+                console.log(res)
+                setDb(JSON.stringify(res.data.db))
+                setLib(JSON.stringify(res.data.lib))
+                setMicroservice(JSON.stringify(res.data.microservice))
+                setKafkaTopic(JSON.stringify(res.data.topic))
+            })
+            .catch(err => {
+                console.log(err)
+            });
+    }
+
     useEffect(() => {
-        async function fetchData() {
-            request
-                .get('/')
-                .then(res => {
-                    console.log(res)
-                    setData(JSON.stringify(res.data))
-                })
-                .catch(err => {
-                    console.log(err)
-                });
-        }
         fetchData()
     }, []);
+
+    const triggerAPI = () => {
+        fetchData()
+    }
 
     const onChange = e => {
         const files = Array.from(e.target.files)
@@ -103,14 +121,19 @@ const Home = props => {
     return (
         <>
             <div className="home-page">
-                <h2>Data</h2>
-                <p>{data}</p>
+                <h2>Server Test</h2>
+                <p><span className="field">DB: </span>{db}</p>
+                <p><span className="field">Python Lib: </span>{lib}</p>
+                <p><span className="field">Microservice: </span>{microservice}</p>
+                <p><span className="field">Kafka Topic: </span>{kafkaTopic}</p>
+                <p><span className="field">Image Upload: </span>{images}</p>
                 <input type='file' id='single' onChange={onChange} /> 
                 {isUploading ? <p>Is Uploading</p> : <p>Is Uploaded</p> }
-                <h2>Images</h2>
-                <p>{images}</p>
+                <button onClick={triggerAPI}>Trigger API</button>
+
+                <h2>Socket Test</h2>
+                <SocketBoard></SocketBoard>
             </div>
-            <Dashboard></Dashboard>
         </>
     )
 }
